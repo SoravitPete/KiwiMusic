dfd97939b7e85500674b047b20fd602262d94630
SoravitPete <esterpiano.official@gmail.com>
2021-11-03 13:13:39 +0700

    update basic model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class SongName(models.Model):
+    song_name = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.song_name
+
+
+class BillBoard(models.Model):
+    billboard = models.CharField(max_length=200)
+    place = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.billboard
+
