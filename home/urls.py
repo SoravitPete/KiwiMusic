44e9dfb3b820f1a4cf752906712c3085086580b0
SoravitPete <esterpiano.official@gmail.com>
2021-11-03 13:32:54 +0700

    Make urls redirect for song detail and billboard.

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('billboard/', views.billboard, name='billboard'),
+    path('<str:song_name>/', views.detail, name='detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Welcome to Home page of KiwiMusic.")
+
+
+def detail(request, song_name):
+    return HttpResponse("You're looking at %s details." % song_name)
+
+
+def billboard(request):
+    return HttpResponse("You're looking at Billboard. \nThis is ranking for this week.")
