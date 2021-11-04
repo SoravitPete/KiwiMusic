f0a60dad50e01dbb8da8836e5204c2026fe33a1a
SoravitPete <esterpiano.official@gmail.com>
2021-11-04 20:13:09 +0700

    try to re-write code for blog page

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', views.ShowBlogHome, name='showblog'),
+    path('room/<str:blog_name>/<str:person_name>', views.ShowBlogPage, name='showblog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, 'blog/index.html', {})
 
 
-def room(request, room_name):
-    return render(request, 'blog/room.html', {
-        'room_name': room_name
-    })
+def ShowBlogHome(request):
+    return HttpResponse("blog Home")
+
+
+def ShowBlogPage(request, blog_name, person_name):
+    # return render(request,"chat_screen.html",{'room_name':room_name,'person_name':person_name})
+    return HttpResponse("Blog page " + blog_name + "" + person_name)
