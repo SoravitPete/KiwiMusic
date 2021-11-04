7a3f5fa9b2ad04b54c3b02c709e4414873bb3278
SoravitPete <esterpiano.official@gmail.com>
2021-11-04 16:30:52 +0700

    make basic urls and structure for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'blog/index.html', {})
+
+
+def room(request, room_name):
+    return render(request, 'blog/room.html', {
+        'room_name': room_name
+    })
