144d3436b2c037150fa0be5b9e26162f7a4a641f
SoravitPete <esterpiano.official@gmail.com>
2021-11-05 01:38:08 +0700

    re-work lol.

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.ShowBlogHome, name='showblog'),
-    path('room/<str:blog_name>/<str:person_name>', views.ShowBlogPage, name='showblog'),
-]
--- kiwimusic/settings.py
+++ kiwimusic/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ASGI_APPLICATION = 'kiwimusic.asgi.application'
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -32,8 +30,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
-    'blog',
     'Home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
