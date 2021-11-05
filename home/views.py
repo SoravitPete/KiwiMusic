43c0640ced46072c006faa19898e72afbff09b01
SoravitPete <esterpiano.official@gmail.com>
2021-11-06 05:37:16 +0700

    just try to pull api lol still doesn't work.

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
+import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import SongName, SongType, BlogName, BlogRoom
+from .models import SongName, SongType, BlogName
 
 
 def index(request):
@@ -23,6 +24,18 @@ def list_song(request, song_type):
 
 
 def details(request, song_type, song_name):
+    # url = "https://unsa-unofficial-spotify-api.p.rapidapi.com/artist"
+    #
+    # querystring = {"id": "1mYsTxnqsietFxj1OgoGbG"}
+    #
+    # headers = {
+    #     'x-rapidapi-host': "unsa-unofficial-spotify-api.p.rapidapi.com",
+    #     'x-rapidapi-key': "480217cf18msh7fbc5f033e28fe5p17bafcjsn5bb4f9f93a60"
+    # }
+    #
+    # response = requests.request("GET", url, headers=headers, params=querystring)
+    #
+    # return HttpResponse(response.text)
     song_type = SongType.objects.get(song_type=song_type)
     song_name = SongName.objects.get(song_name=song_name)
     context = {
@@ -33,7 +46,16 @@ def details(request, song_type, song_name):
 
 
 def billboard(request):
-    return HttpResponse("You're looking at Billboard. \nThis is ranking for this week.")
+    # url = "https://top-10-spotify.p.rapidapi.com/"
+    #
+    # headers = {
+    #     'x-rapidapi-host': "top-10-spotify.p.rapidapi.com",
+    #     'x-rapidapi-key': "480217cf18msh7fbc5f033e28fe5p17bafcjsn5bb4f9f93a60"
+    # }
+    # response = requests.request("GET", url, headers=headers)
+    # return HttpResponse(response.text)
+    # return render(request, "Home/billboard.html", response)
+    return HttpResponse("You are looking at billboard of this week")
 
 
 def blog_home(request):
